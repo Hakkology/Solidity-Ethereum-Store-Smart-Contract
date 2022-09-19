a6814eebfcb1ffc267ab812c8088c55a7edb9814
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-20 00:40:22 +0300

    Add files via upload

--- FeeCollector.sol
+++ FeeCollector.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/*In this case, transfer function is called before the state variable for balance is modified.
+This regrettably leaves "withdraw" function vulnerable to re-entrancy attacks.
+This can easily be prevented by locking the vulnerable function between booleans, as presented below.*/
+
+
 contract FeeCollector {
+    bool private ReentryLock; 
     address public owner;
     uint256 public balance;
 
@@ -16,8 +22,9 @@ contract FeeCollector {
     function withdraw (uint amount, address payable destAddr) public {
         require(msg.sender == owner, "Only owner can withdraw.");
         require(amount <= balance, "Insufficient funds.");
-
+        ReentryLock = true; 
         destAddr.transfer(amount);
         balance -= amount;
+        ReentryLock = false;
     }
 }
