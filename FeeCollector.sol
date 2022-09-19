eef2f74ebfc213898b6548cea5f7bf9abcb66023
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-19 22:38:22 +0300

    Add files via upload

---
+++ FeeCollector.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FeeCollector {
+    address public owner;
+    uint256 public balance;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    receive() payable external {
+        balance += msg.value;
+    }
+
+    function withdraw (uint amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw.");
+        require(amount <= balance, "Insufficient funds.");
+
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
